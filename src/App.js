a07061b0028014704c47a239227c190b8e693589
Sheriff Shittu <possicon@Sheriffs-MacBook-Air.local>
2024-10-29 19:43:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { jobs } from './jobs';
+import logo from "./assets/img/getdev-logo.svg";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  container mx-auto flex justify-center  items-center">
+      <div>
+        <div className='p-4  center'>
+          <img src={logo} alt='GetDev Logo' width={100} height={100} className='center' />
+          <h1 className='text-xl'>GetDev Job Listings</h1>
+          <p className='text-lg'>We've compiled a list of job listings from our clients. </p>
+        </div>
+        {
+          jobs.map((job, key) => (
+            <div key={key} className='my-4  p-2 hover:bg-gray-100 hover:rounded-lg w-3/5'>
+              <div className='flex justify-between items-center'>
+                <div>
+                  <h2 className='font-extrabold'>{job.title}</h2>
+                  <p>Salary: ₦{job.salaryRange}</p>
+                  <p>Category:  {job.category}</p>
+                  <div className='w-3/5 px-2'>
+                    <p className='-ml-2'>Requirements</p>
+                    {
+                      job.requirements.map((item, k) => (
+                        <span key={k} className='text-xs bg-gray-100 p-1 rounded-full space-x-2 mr-1'>{item}</span>
+                      ))
+                    }
+                  </div>
+                </div>
+                <a href={job.applyUrl} target='_blank' className='bg-blue-600 p-3 text-white px-5 rounded-md hover:bg-blue-500 cursor-pointer'>Apply</a>
+              </div>
+              <p>{job.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/jobs.js
@@ -0,0 +1,274 @@
+export const jobs = [
+  {
+    "title": "Intermediate Frontend Developer",
+    "url": "",
+    "category": [
+      "Frontend",
+    ],
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "450-550k",
+    "location": "",
+    "applyUrl": "https://forms.gle/QDScjtc2Dw7KzS9A9",
+    "description": ``,
+    "requirements": [
+      "React",
+      "JavaScript",
+      "Redux",
+      "Node.js",
+      "CSS/Sass",
+      "HTML/CSS",
+      "TypeScript",
+      "Jest",
+      "Enzyme",
+      "Cypress",
+      "React Testing Library",
+      "Storybook",
+      "Material-UI",
+      "Bootstrap",
+      "Firebase",
+    ],
+  },
+  {
+    "title": "Intermediate Microsoft SQL Server Developer ",
+    "url": "",
+    "category": [
+      "Database",
+    ],
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "450-550k",
+    "location": "",
+    "applyUrl": "https://forms.gle/oLm2NDoYenRt61bJA",
+    "description": "",
+    "requirements": [
+      "Microsoft SQL Server",
+      "T-SQL",
+      "SQL Server Management Studio",
+      "Database Design",
+      "Database Architecture",
+      "Database Performance",
+      "Database Security",
+      "Database Backup and Recovery",
+      "Database Development",
+      "Database Maintenance",
+      "Database Administration",
+      "Database Integration",
+    ],
+  },
+  {
+    "title": "Senior UX Designer",
+    "url": "",
+    "category": "Product Design",
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "600 - 800k",
+    "location": "",
+    "applyUrl": "https://forms.gle/843mMrDzGxJWg53q8",
+    "description": "",
+    "requirements": [
+      "User Experience",
+      "User Interface",
+      "User Experience Design",
+      "Visual Design",
+      "Branding",
+      "Interaction Design",
+    ],
+  },
+  {
+    "title": "Intermediate React Native Developer",
+    "url": "",
+    "category": [
+      "Mobile Developer",
+      "Hybrid Mobile Developer"
+    ],
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "450 - 550k",
+    "location": "",
+    "applyUrl": "https://forms.gle/dB7zXVhRmfoRFERn6",
+    "description": "",
+    "requirements": [
+      "React Native",
+      "JavaScript",
+      "Redux",
+      "Node.js",
+      "CSS/Sass",
+      "HTML/CSS",
+      "TypeScript",
+      "Jest",
+      "Enzyme",
+      "Cypress",
+      "React Testing Library",
+      "Firebase",
+    ],
+  },
+  {
+    "title": "Intermediate Quality Assurance Engineer ",
+    "url": "",
+    "category": "",
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "450-550k",
+    "location": "",
+    "applyUrl": "https://forms.gle/oR54EfFMPCCGm1gV7 ",
+    "description": "",
+    "requirements": [
+      "Quality Assurance",
+      "Test Automation",
+      "Software Development",
+      "Software Testing",
+      "User Acceptance Testing",
+      "Test Management",
+      "Test Planning",
+      "Test Design",
+      "Test Execution",
+      "Test Reporting",
+    ],
+  },
+  {
+    "title": "Intermediate Backend Developer ",
+    "url": "",
+    "category": "",
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "450-550k",
+    "location": "",
+    "applyUrl": "https://forms.gle/dgGmt7kFk2bbAbow9 ",
+    "description": "",
+    "requirements": [
+      "ASP.NET",
+      "SQL Server",
+      "MySQL",
+    ],
+  },
+  {
+    "title": "Frontend Developer",
+    "url": "",
+    "category": "",
+    "jobType": [
+      // "Full-Time",
+      // "Part-Time",
+      "Contract"
+    ],
+    "jobMode": [
+      // "Remote",
+      // "On-site",
+      "Hybrid"
+    ],
+    "salaryRange": "250-350k",
+    "location": "",
+    "applyUrl": "https://forms.gle/CB83PwCTCkqsTRkk6",
+    "description": "",
+    "requirements": [
+      "React",
+      "JavaScript",
+      "Redux",
+      "Node.js",
+      "CSS/Sass",
+      "HTML/CSS",
+      "TypeScript",
+    ],
+  },
+  {
+    "title": "Senior Frontend Developer ",
+    "url": "",
+    "category": "",
+    "jobType": [
+      "Full-Time",
+      // "Part-Time",
+      // "Contract"
+    ],
+    "jobMode": [
+      "Remote",
+      // "On-site",
+      // "Hybrid"
+    ],
+    "salaryRange": "600-800k",
+    "location": "",
+    "applyUrl": "https://forms.gle/raHffFiqhAi1vXaj7",
+    "description": "",
+    "requirements": [
+      "React",
+      "JavaScript",
+      "Redux",
+      "Node.js",
+      "CSS/Sass",
+      "HTML/CSS",
+      "TypeScript",
+      "Jest",
+      "Enzyme",
+      "Cypress",
+      "React Testing Library",
+      "Firebase",
+    ],
+  },
+  // {
+  //   "title": "",
+  //   "url": "",
+  //   "category": "",
+  //   "jobType": [
+  //     "Full-Time",
+  //     "Part-Time",
+  //     "Contract"
+  //   ],
+  //   "jobMode": [
+  //     "Remote",
+  //     "On-site",
+  //     "Hybrid"
+  //   ],
+  //   "salaryRange": "",
+  //   "location": "",
+  //   "applyUrl": "",
+  //   "description": "",
+  //   "requirements": "",
+  // },
+];
