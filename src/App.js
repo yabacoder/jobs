9378e4eb110af8337535d052ae97439398aea9bb
Sheriff Shittu <possicon@Sheriffs-MacBook-Air.local>
2024-10-31 14:20:42 +0100

    fixed responsiveness

--- src/App.js
+++ src/App.js
@@ -4,37 +4,44 @@ import logo from "./assets/img/getdev-logo.svg";
 function App() {
 
   return (
-    <div className="App  container mx-auto flex justify-center  items-center">
-      <div>
-        <div className='p-4  center'>
-          <img src={logo} alt='GetDev Logo' width={100} height={100} className='center' />
-          <h1 className='text-xl'>GetDev Job Listings</h1>
-          <p className='text-lg'>We've compiled a list of job listings from our clients. </p>
+    <div className="flex items-center justify-center w-full p-2 lg:w-3/5">
+      <div className='m-2'>
+        <div className='flex items-center justify-center'>
+          <div className=''>
+            <img src={logo} alt='GetDev Logo' width={100} height={100} className='mx-auto' />
+            <h1 className='text-xl text-center'>GetDev Job Listings</h1>
+            <p className='text-lg text-center'>We've compiled a list of job listings from our clients. </p>
+          </div>
         </div>
-        {
-          jobs.map((job, key) => (
-            <div key={key} className='my-4  p-2 hover:bg-gray-100 hover:rounded-lg w-3/5'>
-              <div className='flex justify-between items-center'>
-                <div>
-                  <h2 className='font-extrabold'>{job.title}</h2>
-                  <p>Salary: ₦{job.salaryRange}</p>
-                  <p>Category:  {job.category}</p>
-                  <div className='w-3/5 px-2'>
-                    <p className='-ml-2'>Requirements</p>
-                    {
-                      job.requirements.map((item, k) => (
-                        <span key={k} className='text-xs bg-gray-100 p-1 rounded-full space-x-2 mr-1'>{item}</span>
-                      ))
-                    }
+        <div>
+          {
+            jobs.map((job, k) => (
+              <div className='w-full p-2 m-4 hover:bg-gray-100 hover:rounded-lg'>
+                <div className='flex items-center justify-between w-full'>
+                  <div>
+                    <h2 className='font-extrabold'>{job.title}</h2>
+                    <p>Salary: ₦{job.salaryRange}</p>
+                    <p>Category:  {job.category}</p>
+                    <div className='flex-wrap w-2/3 '>
+                      <p className=''>Requirements</p>
+                      {
+                        job.requirements.slice(0, 5).map((item, k) => (
+                          <span key={k} className='p-1 mr-1 space-x-2 text-xs bg-gray-100 rounded-full'>{item}</span>
+                        ))
+                      }
+                    </div>
+                  </div>
+                  <div>
+                    <a href='' target='_blank' className='p-3 px-5 text-white bg-blue-600 rounded-md cursor-pointer hover:bg-blue-500'>Apply</a>
                   </div>
                 </div>
-                <a href={job.applyUrl} target='_blank' className='bg-blue-600 p-3 text-white px-5 rounded-md hover:bg-blue-500 cursor-pointer'>Apply</a>
               </div>
-              <p>{job.description}</p>
-            </div>
-          ))
-        }
+            ))
+          }
+        </div>
+
       </div>
+
     </div>
   );
 }
